e87004879c853a0c8e566e06706c88b330093d15
ash <ashutosh.bex74004@kecktm.edu.np>
2024-04-23 03:10:08 +0545

    [added]

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,8 +73,8 @@ resource "aws_instance" "aws_ins_web" {
     - apt install docker-compose-plugin -y
     - curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     - chmod +x /usr/local/bin/docker-compose
-    - apt install docker -y
-    - apt install docker-compose-plugin -y
+    - apt install docker 
+    - apt install docker-compose-plugin 
     - usermod -aG Docker ubuntu
     - systemctl start docker
     - systemctl enable docker
